d05af5812894b9ae4466abcd846944a8085a23c4
Rohit Sharma <rohits8731@gmail.com>
2026-03-03 20:46:28 +0530

    27: Execution History

--- src/app/(dashboard)/(rest)/executions/page.tsx
+++ src/app/(dashboard)/(rest)/executions/page.tsx
@@ -1,13 +1,34 @@
+import { ExecutionsContainer, ExecutionsError, ExecutionsList, ExecutionsLoading } from '@/features/executions/components/executions';
+import { executionsParamsLoader } from '@/features/executions/server/params-loader';
+import { prefetchExecutions } from '@/features/executions/server/prefetch';
 import { requireAuth } from '@/lib/auth-utils'
-import React from 'react'
+import { HydrateClient } from '@/trpc/server';
+import { SearchParams } from 'nuqs';
+import React, { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary';
 
-const ExecutionPage = async () => {
+type Props = {
+    searchParams: Promise<SearchParams>;
+};
+
+const ExecutionPage = async ({ searchParams }: Props) => {
 
     await requireAuth();
 
+    const params = await executionsParamsLoader(searchParams);
+    prefetchExecutions(params);
+
     return (
-        <div>ExecutionPage</div>
-    )
+        <ExecutionsContainer>
+            <HydrateClient>
+                <ErrorBoundary fallback={<ExecutionsError />}>
+                    <Suspense fallback={<ExecutionsLoading />}>
+                        <ExecutionsList />
+                    </Suspense>
+                </ErrorBoundary>
+            </HydrateClient>
+        </ExecutionsContainer>
+    );
 }
 
 export default ExecutionPage
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,7 +2,7 @@ import { NonRetriableError } from "inngest";
 import { inngest } from "./client";
 import prisma from "@/lib/db";
 import { topologicalSort } from "./utils";
-import { NodeType } from "@/generated/prisma/enums";
+import { ExecutionStatus, NodeType } from "@/generated/prisma/enums";
 import { getExecutor } from "@/features/executions/lib/executor-registry";
 import { httpRequestChannel } from "./channels/http-request";
 import { manualTriggerChannel } from "./channels/manual-trigger";
@@ -16,6 +16,16 @@ export const executeWorkflow = inngest.createFunction(
     { 
         id: "execute-workflow",
         retries: process.env.NODE_ENV === "production" ? 3 : 0,
+        onFailure: async ({ event, step }) => {
+            return prisma.execution.update({
+                where: { inngestEventId: event.data.event.id },
+                data: {
+                    status: ExecutionStatus.FAILED,
+                    error: event.data.error.message,
+                    errorStack: event.data.error.stack,
+                },
+            });
+        },
     },
     { 
         event: "workflows/execute.workflow",
@@ -30,10 +40,19 @@ export const executeWorkflow = inngest.createFunction(
         ]
     },
     async ({ event, step, publish }) => {
+        const inngestEventId = event.id;
         const workflowId = event.data.workflowId;
-        if(!workflowId) {
-            throw new NonRetriableError("Workflow ID is Missing")
+        if (!inngestEventId || !workflowId) {
+            throw new NonRetriableError("Event ID or workflow ID is missing");
         }
+        await step.run("create-execution", async () => {
+            return prisma.execution.create({
+                data: {
+                    workflowId,
+                    inngestEventId,
+                },
+            });
+        });
         const sortedNodes = await step.run("prepare-workflow", async () => {
             const workflow = await prisma.workflow.findUniqueOrThrow({
                 where: {
@@ -67,6 +86,16 @@ export const executeWorkflow = inngest.createFunction(
                 publish,
             })
         }
+        await step.run("update-execution", async () => {
+            return prisma.execution.update({
+                where: { inngestEventId, workflowId },
+                data: {
+                    status: ExecutionStatus.SUCCESS,
+                    completedAt: new Date(),
+                    output: context,
+                },
+            })
+        });
         return { workflowId, result: context }
     }
 )
